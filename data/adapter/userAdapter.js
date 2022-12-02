327225e0a3d8dc84c3e2774fdc5c3a29821d2577
= <=>
2022-12-01 21:02:30 -0800

    clean-up: abstract class for user adapter

---
+++ data/adapter/userAdapter.js
@@ -0,0 +1,18 @@
+/**
+ * This is an abstract class for implementing User Adapter
+ */
+class UserAdapter {
+  constructor() {
+    if (this.constructor === UserAdapter)
+      throw new TypeError(`cannot instantiate an abstract class`);
+  }
+
+  getAllUsers() {}
+  getUser(username) {}
+  createUser(username, email, password) {}
+  deleteUser(username) {}
+}
+
+exports = {
+  UserAdapter,
+};
